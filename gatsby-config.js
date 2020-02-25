3f7402f02b6341a9d4034aa52451ffc42f6c5f3e
Andreas Åkerlöf <a.akerlof@gmail.com>
2020-02-25 09:18:35 +0100

    Removed DatoCMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,5 @@
-require("dotenv").config({path: '.env'});
-
 module.exports = {
   plugins: [
-    {
-      resolve: `gatsby-source-datocms`,
-      options: {
-        apiToken: process.env.DATOCMS_TOKEN,
-        previewMode: false,
-        disableLiveReload: false,
-      }
-    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-theme-ui`
   ]
