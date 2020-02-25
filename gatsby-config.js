7d0b1c7a4ea23dc7c01790a3fe7dbbe0b165638b
Andreas Åkerlöf <a.akerlof@gmail.com>
2020-02-25 11:40:50 +0100

    added wordpress

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-theme-ui`
+    `gatsby-plugin-theme-ui`,
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        baseUrl: `akerlof.esy.es`,
+        protocol: `http`,
+        hostingWPCOM: false,
+        useACF: false,
+      }
+    }
   ]
 }
