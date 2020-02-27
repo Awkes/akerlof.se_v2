e4e02be4837ca03f2428f5c5a5070dc1a78b4dee
Andreas Åkerlöf <a.akerlof@gmail.com>
2020-02-27 14:48:47 +0100

    added title and contents meta tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { graphql } from 'gatsby';
 import { Global } from '@emotion/core';
+import Helmet from 'react-helmet';
+import PropTypes from 'prop-types';
 import Layout from '../components/Layout';
 import Section from '../components/Section';
 
-export default ({
-  data: { wordpressWpSite: { acf: { background_image: { source_url: bgImg } }}}
-}) => (
+const Index = ({
+  data: { 
+    wordpressSiteMetadata: { name, description, home },
+    wordpressWpSite: { acf: { background_image: { source_url: bgImg } }}
+  }
+}) => {
+  const url = new URL(home);
+  return (
     <>
+      <Helmet htmlAttributes={{lang: 'en'}}>
+        <title>{name} - {url.hostname+(url.pathname !== '/' ? url.pathname : '')}</title>
+        <meta name="description" content={description} />
+      </Helmet>
       <Global styles={{
         body: {
           backgroundImage: `url(${bgImg})`,
@@ -30,9 +41,21 @@ export default ({
       </Layout>
     </>
   );
+}
+
+export default Index;
+
+Index.propTypes = {
+  data: PropTypes.object.isRequired
+}
 
 export const query = graphql`
   query {
+    wordpressSiteMetadata {
+      description
+      name
+      home
+    }
     wordpressWpSite(title: {eq: "Settings"}) {
       acf {
         background_image {
@@ -42,3 +65,4 @@ export const query = graphql`
     }
   }
 `;
+
