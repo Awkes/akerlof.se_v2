8cf73e4b36eb1a01d3cee313d439fbe89c026c91
Andreas Åkerlöf <a.akerlof@gmail.com>
2020-02-27 14:11:04 +0100

    footer completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,41 @@
 /** @jsx jsx */
-import { jsx } from "theme-ui";
-import Nav from "../components/Nav";
+import { jsx } from 'theme-ui';
+import { useStaticQuery, graphql } from 'gatsby';
+import HTMLReactParser from 'html-react-parser'
+import Nav from './Nav';
+import SocialMediaIcons from './SocialMediaIcons'
 
-export default({children}) => (
-  <footer sx={{backgroundColor: 'footer'}}>
-    <Nav /*className={styles.nav}*/ />
+const Footer = () => {
+  const { wordpressWpSite: { acf: { text } }} = useStaticQuery(graphql`
+    query {
+      wordpressWpSite(title: {eq: "Footer"}) {
+        acf {
+          text
+        }
+      }
+    }
+  `);
 
-    <div>
-      Copyright &copy; Andreas Åkerlöf 2019
-    </div>
-  </footer>
-);
+  return (
+    <footer sx={{
+      backgroundColor: 'footer',
+      padding: 3,
+      paddingBottom: 4,
+      textAlign: 'center',
+      borderTop: 'darkBorder',
+      '&>nav': {
+        display: 'inline-block',
+        '&>ul>li': {
+          display: ['initial', 'inline-block'],
+          paddingX: [0, 3]
+        }
+      }
+    }}>
+      <Nav />
+      <div sx={{ margin: 3 }}><SocialMediaIcons /></div>
+      <div>{HTMLReactParser(text)}</div>
+    </footer>
+  );
+}
+
+export default Footer;
